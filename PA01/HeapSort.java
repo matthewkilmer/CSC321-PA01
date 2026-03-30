68e80012838ae9a29eea47396843a253c4dba5ea
matthewkilmer <mattkilmer5@gmail.com>
2026-03-29 20:21:25 -0400

    second commit, brushed up on logic and added comprehensive comments

--- PA01/HeapSort.java
+++ PA01/HeapSort.java
@@ -4,7 +4,7 @@ package PA01;
 // create our HeapSort class
 public class HeapSort {
 
-    // define the main functionality
+    // define the heapSort function
     public static void heapSort(String[] A, int order) {
         int n = A.length;
 
@@ -15,22 +15,27 @@ public class HeapSort {
             A[i] = A[0];
             A[0] = temp;
 
-            maxHeapify(A, 0, i, order);
+            heapify(A, 0, i, order);
         }
     }
 
-    // define our BuildMaxHeap and MaxHeapify functions
+    // define our buildHeap function
     public static void buildHeap(String[] A, int n, int order) {
         for (int i = Math.floorDiv(n, 2); i >= 0; i--) {
-            maxHeapify(A, i, n, order);
+            heapify(A, i, n, order);
         }
     }
 
-    public static void maxHeapify(String[] A, int i, int n, int order) {
+
+    // define a heapify function
+    public static void heapify(String[] A, int i, int n, int order) {
+        
+        // inititalize vars
         int l = 2*i + 1;
         int r = 2*i + 2;
         int extreme = i;
 
+        // logic for ascending order
         if (order == 0) {
 
             if (l < n && A[l].compareTo(A[extreme]) > 0) {
@@ -41,6 +46,8 @@ public class HeapSort {
                 extreme = r;
             }
 
+
+        // logic for descending order
         } else {
 
             if (l < n && A[l].compareTo(A[extreme]) < 0) {
@@ -57,17 +64,21 @@ public class HeapSort {
             String temp = A[i];
             A[i] = A[extreme];
             A[extreme] = temp;
-            maxHeapify(A, extreme, n, order);
+            heapify(A, extreme, n, order);
         }
     }
 
+    // define a function to print an array so we can present our results
     public static void printArray(String[] A) {
+
         for (String val : A) System.out.print(val + " ");
         System.out.println();
     }
 
+    // define a main function to call our heapSort function
     public static void main(String[] args) {
         
+        // initialize a list of strings to test our heapSort function
         String[] A = {"celtics", "commanders", "mystics", "spartans", "owls"};
 
         heapSort(A, 0); // ascending
